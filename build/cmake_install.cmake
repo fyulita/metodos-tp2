0bf75e850e450c290bf39dcea394f3f15409ba1e
fyulita <federico_yulita@hotmail.com>
2020-05-25 18:13:45 -0300

    Hice el build.

---
+++ build/cmake_install.cmake
@@ -0,0 +1,79 @@
+# Install script for directory: /home/fyulita/Documents/Fede/UBA/2020-1C/Metodos Numericos/Material/Laboratorio/tp02
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "/home/fyulita/Documents/Fede/UBA/2020-1C/Metodos Numericos/Material/Laboratorio/tp02")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "Release")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Install shared libraries without execute permission?
+if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  set(CMAKE_INSTALL_SO_NO_EXE "1")
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "FALSE")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  if(EXISTS "$ENV{DESTDIR}/home/fyulita/Documents/Fede/UBA/2020-1C/Metodos Numericos/Material/Laboratorio/tp02/notebooks/metnum.cpython-38-x86_64-linux-gnu.so" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}/home/fyulita/Documents/Fede/UBA/2020-1C/Metodos Numericos/Material/Laboratorio/tp02/notebooks/metnum.cpython-38-x86_64-linux-gnu.so")
+    file(RPATH_CHECK
+         FILE "$ENV{DESTDIR}/home/fyulita/Documents/Fede/UBA/2020-1C/Metodos Numericos/Material/Laboratorio/tp02/notebooks/metnum.cpython-38-x86_64-linux-gnu.so"
+         RPATH "")
+  endif()
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "/home/fyulita/Documents/Fede/UBA/2020-1C/Metodos Numericos/Material/Laboratorio/tp02/notebooks/metnum.cpython-38-x86_64-linux-gnu.so")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+file(INSTALL DESTINATION "/home/fyulita/Documents/Fede/UBA/2020-1C/Metodos Numericos/Material/Laboratorio/tp02/notebooks" TYPE MODULE FILES "/home/fyulita/Documents/Fede/UBA/2020-1C/Metodos Numericos/Material/Laboratorio/tp02/build/metnum.cpython-38-x86_64-linux-gnu.so")
+  if(EXISTS "$ENV{DESTDIR}/home/fyulita/Documents/Fede/UBA/2020-1C/Metodos Numericos/Material/Laboratorio/tp02/notebooks/metnum.cpython-38-x86_64-linux-gnu.so" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}/home/fyulita/Documents/Fede/UBA/2020-1C/Metodos Numericos/Material/Laboratorio/tp02/notebooks/metnum.cpython-38-x86_64-linux-gnu.so")
+    if(CMAKE_INSTALL_DO_STRIP)
+      execute_process(COMMAND "/usr/bin/strip" "$ENV{DESTDIR}/home/fyulita/Documents/Fede/UBA/2020-1C/Metodos Numericos/Material/Laboratorio/tp02/notebooks/metnum.cpython-38-x86_64-linux-gnu.so")
+    endif()
+  endif()
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  include("/home/fyulita/Documents/Fede/UBA/2020-1C/Metodos Numericos/Material/Laboratorio/tp02/build/pybind11/cmake_install.cmake")
+
+endif()
+
+if(CMAKE_INSTALL_COMPONENT)
+  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
+else()
+  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
+endif()
+
+string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
+       "${CMAKE_INSTALL_MANIFEST_FILES}")
+file(WRITE "/home/fyulita/Documents/Fede/UBA/2020-1C/Metodos Numericos/Material/Laboratorio/tp02/build/${CMAKE_INSTALL_MANIFEST}"
+     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
